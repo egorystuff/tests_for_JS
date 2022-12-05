66c02541ab703640c70b926f16d3efd1e6f02260
egorystuff <mr.yankovec@gmail.com>
2022-12-05 20:37:07 +0300

    Update promt.js

--- promt.js
+++ promt.js
@@ -1,2 +1,14 @@
-let name = prompt('Как вас зовут?');
-console.log('Привет, ' + name);
+// let name = prompt('Как вас зовут?');
+// console.log('Привет, ' + name);
+
+// let likesCats = confirm('Тебе нравятся кошки?');
+// if (likesCats) {
+//   console.log('Ты классная кошка!');
+// } else {
+//   console.log('Что ж, не проблема, все равно ты молодец!');
+// }
+
+// alert('JS это очень здорово!');
+
+var words = ['программа', 'макака', 'прекрасный', 'оладушек'];
+var word = words[Math.floor(Math.random() * words.length)];
