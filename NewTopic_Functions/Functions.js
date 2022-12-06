4c706fd4443ffe859bd2d6e64cf259901584c406
egorystuff <mr.yankovec@gmail.com>
2022-12-06 22:12:24 +0300

    NewTopic_Functions

---
+++ NewTopic_Functions/Functions.js
@@ -0,0 +1,25 @@
+//просто пишет текст
+var ourFirstFunction = function () {
+  console.log('Привет, мир!');
+};
+
+//функция с аргументом
+var sayHelloTo = function (name) {
+  console.log('Привет, ' + name + '!');
+};
+
+//функция с циклом, повторяет цикл столько раз, сколько укажешь в аргументе при вызове функции
+var drawCats = function (howManyTimes) {
+  for (var i = 0; i < howManyTimes; i++) {
+    console.log(i + ' =^.^=');
+  }
+};
+
+//функция с двумя аргументами. Функция printMultipleTimes печатает строку, переданную в аргументе whatToDraw столько раз, сколько указано в аргументе howManyTimes. Второй аргумент сообщает функции, что печатать, а первый — сколько раз это нужно печатать.
+var printMultipleTimes = function (howManyTimes, whatToDraw) {
+  for (var i = 0; i < howManyTimes; i++) {
+    console.log(i + ' ' + whatToDraw);
+  }
+};
+
+console.log(printMultipleTimes(4, '=^.^='));
