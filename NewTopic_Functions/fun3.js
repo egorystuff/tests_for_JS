3b5e2db9bfdbb238a10ec800354912e644d56c7b
egorystuff <mr.yankovec@gmail.com>
2022-12-07 21:49:44 +0300

    Update fun3.js

--- NewTopic_Functions/fun3.js
+++ NewTopic_Functions/fun3.js
@@ -1,3 +1,4 @@
+//упражнение 1
 var add = function (number1, number2) {
   return number1 + number2;
 };
@@ -7,3 +8,11 @@ var multiply = function (number3, number4) {
 };
 var result = multiply(36325, 9824) + add(777, 0);
 console.log(result);
+
+//упражнение 2
+var areArraysSame = function (array1, array2) {
+  while (array1 !== array2) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {}
+};
