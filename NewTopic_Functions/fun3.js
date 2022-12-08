a4c87575662e3707bd40bf7f198e9a441c397d6a
Yahor Yankavets <mr.yankovec@gmail.com>
2022-12-08 15:19:09 +0300

    Update fun3.js

--- NewTopic_Functions/fun3.js
+++ NewTopic_Functions/fun3.js
@@ -10,15 +10,17 @@ var result = multiply(36325, 9824) + add(777, 0);
 console.log(result);
 
 //упражнение 2
-var areArraysSame = function (array1, array2) {
-  if (array1.lenght != array2.lenght) {
+var areArraysSame = function (arr1, arr2) {
+  if (arr1.lenght !== arr2.lenght) {
     return false;
-  } else {
-    for (let i = 0; i < array1.length; i++) {
-      array1[i] === array2[i];
-    }
-    return true;
   }
+
+  for (var i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
 };
 
-console.log(areArraysSame([1, 2, 3], [1, 2, 3, 4]));
+console.log(areArraysSame([1, 2, 3], [1, 2, 3, 4, 5]));
